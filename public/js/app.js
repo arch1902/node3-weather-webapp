7bca960e22d17aeea69380c79a4aaa202fe56daa
finvic <pranshu@Yogeshs-MacBook-Air.local>
2020-10-04 16:35:46 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,15 @@
-console.log('client side js file')
+// console.log('client side js file')
 
-fetch('http://localhost:3000/weather?address=!').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        }else{
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
+// fetch('http://localhost:3000/weather?address=!').then((response)=>{
+//     response.json().then((data)=>{
+//         if(data.error){
+//             console.log(data.error)
+//         }else{
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -25,7 +25,7 @@ weatherForm.addEventListener('submit',(e)=>{
     messageOne.textContent='Loading...'
     messageTwo.textContent =''
 
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent=data.error
